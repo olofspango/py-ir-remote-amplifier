39d4b37a6d9d9af59cedd133ad0d015feb98b73c
Olof S <olof.spango@gmail.com>
2020-04-24 11:06:20 +0200

    added all codes from RAS13 remote

--- pyremote.py
+++ pyremote.py
@@ -6,6 +6,8 @@ import serial
 
 # CONSTANTS
 COMMANDS = {
+    "CD_POWER": "9E6106F9",
+    "PURE_DIRECT":"5EA1BB44",
     "LINE1" : "5EA19867",
     "LINE2" : "5EA1837C",
     "LINE3" :   "5EA103FC",
@@ -15,17 +17,25 @@ COMMANDS = {
     "PHONO" :   "5EA128D7",
     "CD" :   "5EA1A857",    #  Was: CD
     "BAND" :   "5EA1758A",
+    "TUNING_DOWN": "FE8026D8",
+    "TUNING_UP": "FE808678",
+    "MEMORY": "5EA1F50A",
+    "PRESET_DOWN": "5EA18877",
+    "PRESET_UP": "5EA108F7",
+    "DISK_SKIP":"9E61F20D",
     "VOLUMEUP" :   "5EA158A7",       #  Was: VOLUME_PLUS
     "VOLUMEDOWN" :   "5EA1D827",
     "BAND" :      "7E81758A",
     "STANDBY" :      "7E8154AB",
     "PLAY" :      "9E6140BF",
+    "PAUSE": "9E61AA55",
     "STOP" :      "9E616A95",
+    "NEXT": "9E61E01F",
+    "PREV":"9E6120DF",
     "OPEN_CLOSE" :      "9E61807F",
     "MUTE": "5EA138C7",
-    "NEXT" : "9E61609F",
-    "PREV": "9E61A05F",
-    "PAUSE": "9E61AA55"
+    "FORWARD" : "9E61609F",
+    "REWIND": "9E61A05F"
 }
 s = serial.Serial('/dev/ttyUSB0', 9600)
 app = Flask(__name__)
